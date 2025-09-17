49f71a4071512fd66c3065309f899e3e7c0893a5
Torab-spec <torabpwskill@gmail.com>
2025-09-17 15:40:17 +0530

    lec05

--- src/CWH_Lec_05.java
+++ src/CWH_Lec_05.java
@@ -1,5 +1,17 @@
 public class CWH_Lec_05 {
     public static void main(String[] args) {
-        System.out.println("Hello tora");
+        byte age = 34;
+        char ch = 'A';
+        short age2 = 984;
+        float f1 = 5.6f;
+        double d1 = 4.66d;
+        long ageDino = 5343434434343L;
+        boolean isTrue = false;
+        String str = "Tora";
+        System.out.println(isTrue);
+        System.out.println(age);
+        System.out.println(ageDino);
+        System.out.println(str);
+        System.out.println(ch);
     }
 }
