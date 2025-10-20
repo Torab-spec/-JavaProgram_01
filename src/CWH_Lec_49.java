0b87ac3a007a9c1d50e33b8b1af2dc09f2c97b92
Torab-spec <torabpwskill@gmail.com>
2025-10-20 22:57:53 +0530

    lec 49

--- src/CWH_Lec_49.java
+++ src/CWH_Lec_49.java
@@ -1,8 +1,49 @@
 
+class SupPhone {
+    public void alarm() {
+        System.out.println("Alarm is ringing from SupPhone...");
+    }
+
+    public void call() {
+        System.out.println("Calling from SupPhone...");
+    }
+
+}
+
+class SmartPhone extends SupPhone {
+    @Override
+    public void call() {
+        System.out.println("Calling from SmartPhone...");
+    }
+
+    public void music() {
+        System.out.println("Music playing from SmartPhone...");
+    }
+
+}
 
 
 public class CWH_Lec_49 {
     public static void main(String[] args) {
+
+        SupPhone supPhone = new SupPhone();
+
+//        supPhone.alarm();
+//        supPhone.call();
+//        System.out.println(" ");
+
+        SmartPhone smartPhone = new SmartPhone();
+
+//        smartPhone.alarm();
+//        smartPhone.call();
+//        smartPhone.music();
+
+        SupPhone dmd = new SmartPhone();
+
+        dmd.alarm();
+        dmd.call();
+//        dmd.music(); // Not Accepted
+
 
     }
 }
